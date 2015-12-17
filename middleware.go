a426aff6f9287fe1a7cf28938d43cbb7b09fba16
Louis Andris <mail@louisandris.com>
2015-12-17 07:16:27 +0000

    changes

--- middleware.go
+++ middleware.go
@@ -8,58 +8,58 @@ import (
 type Middleware struct {
 	formName   string
 	cookieName string
-	languages  []string
+	locales    []string
 }
 
-var availableLanguages []string = []string{
+var availableLocales []string = []string{
 	"en-US",
 	"id-ID",
 }
 
 func New() *Middleware {
-	return NewCustomName("language", "language")
+	return NewCustomName("locale", "locale")
 }
 
-func NewCustomLanguage(l []string) *Middleware {
-	m := NewCustomName("language", "language")
-	m.SetLanguages(l)
+func NewCustomLocales(l []string) *Middleware {
+	m := NewCustomName("locale", "locale")
+	m.SetLocales(l)
 	return m
 }
 
 func NewCustomName(f, c string) *Middleware {
-	return &Middleware{formName: f, cookieName: c, languages: availableLanguages}
+	return &Middleware{formName: f, cookieName: c, locales: availableLocales}
 }
 
-func (m *Middleware) SetLanguages(l []string) error {
+func (m *Middleware) SetLocales(l []string) error {
 	if len(l) < 1 {
-		return fmt.Errorf("no language supplied")
+		return fmt.Errorf("no locale supplied")
 	}
-	m.languages = l
+	m.locales = l
 	return nil
 }
 
 func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	l := r.FormValue(m.formName)
 	if l != "" {
-		r.Form.Set(m.formName, m.getLanguage(w, l))
+		r.Form.Set(m.formName, m.getLocale(w, l))
 		next(w, r)
 		return
 	}
 
 	c, err := r.Cookie(m.cookieName)
 	if err != nil {
-		r.Form.Set(m.formName, m.getLanguage(w, m.languages[0]))
+		r.Form.Set(m.formName, m.getLocale(w, m.locales[0]))
 		next(w, r)
 		return
 	}
 
-	r.Form.Set(m.formName, m.getLanguage(w, c.Value))
+	r.Form.Set(m.formName, m.getLocale(w, c.Value))
 	next(w, r)
 }
 
-func (m *Middleware) getLanguage(w http.ResponseWriter, l string) string {
-	tl := m.languages[0]
-	for _, v := range m.languages {
+func (m *Middleware) getLocale(w http.ResponseWriter, l string) string {
+	tl := m.locales[0]
+	for _, v := range m.locales {
 		if l == v {
 			tl = l
 			break
@@ -67,7 +67,7 @@ func (m *Middleware) getLanguage(w http.ResponseWriter, l string) string {
 	}
 
 	http.SetCookie(w, &http.Cookie{
-		Name: m.cookieName,
+		Name:  m.cookieName,
 		Value: tl,
 	})
 	return tl
